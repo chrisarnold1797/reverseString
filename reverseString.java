7b8894deeb038cd2d83e9765f115531a831338b7
ChrisArnold <110760251+chrisarnold1797@users.noreply.github.com>
2024-04-03 17:30:01 -0700

    Add input method reverseString.java
    Allow the program to prompt user for input to avoid editing the file to change the string.

--- reverseString.java
+++ reverseString.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class reverseString {
-    
+
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String userInput = scanner.nextLine();
+
     public static void main(String[] args) {
-        String originalStr = "Hello";
+        String originalStr = userInput;
         String reversedStr = "";
         for (int i = 0; i < originalStr.length(); i++) {
             reversedStr = originalStr.charAt(i) + reversedStr;
